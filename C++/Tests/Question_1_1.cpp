00aca29c5d8767c909ad36455c2a33438ebe074e
Roberto Rodriguez <crazy_kalabaza@yahoo.com>
2016-05-24 14:50:26 -0500

    Refactor of the code for C++, the test and code of the questions are on the same project. Removal of unused project.

--- C++/Tests/Question_1_1.cpp
+++ C++/Tests/Question_1_1.cpp
@@ -1,7 +1,37 @@
 #include <gtest/gtest.h>
+#include <string>
 
-// Forward declaration, the method is on the lib file of the C++ project
-bool hasUniqueChars(const std::string&);
+// Question 1.1:
+// Implement an algorithm to determine if a string has all unique characters. What
+// if you cannot use additional data structures?
+
+bool hasUniqueChars(const std::string &text)
+{
+    // Considering that only ASCII characters are going to be received on the string
+    // an array of 256 positions is enough to tell if there are any repeated
+    // chars in the input string.
+    bool charArray[256] = {};
+
+    // If more than 256 are received on the input string it means that at least one
+    // character is repeated.
+    if (text.size() >= 256)
+        return false;
+
+    // Take each letter of the input text and mark the position on the array for that
+    // specific letter, if the position was already marked it means that a duplicated
+    // character was found.
+    for (auto &c : text)
+    {
+        // Return false to indicate that a character was repeated in the text.
+        if (charArray[c] == true)
+            return false;
+        // Mark the position on the array.
+        charArray[c] = true;
+    }
+
+    // If there was not a repeated character, return true.
+    return true;
+}
 
 // Test cases to ensure the solution to the question is correct
 
